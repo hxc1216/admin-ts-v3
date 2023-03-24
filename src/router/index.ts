c29d5459d139338c34e779b10c965a18c7bb5397
HouXinchao <860618589@qq.com>
2023-03-24 17:41:38 +0800

    build: env public

--- src/router/index.ts
+++ src/router/index.ts
@@ -47,4 +47,5 @@ const router = createRouter({
   ]
 })
 
+
 export default router
--- vite.config.ts
+++ vite.config.ts
@@ -1,29 +1,56 @@
 import { fileURLToPath, URL } from 'node:url'
-import path, { resolve } from "path"
-import { defineConfig } from 'vite'
+import path, { resolve } from 'path'
+import { loadEnv, type ConfigEnv, type UserConfigExport } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 import { createSvgIconsPlugin } from 'vite-plugin-svg-icons'
 import svgLoader from 'vite-svg-loader'
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  resolve: {
-    alias: {
-      // '@': fileURLToPath(new URL('./src', import.meta.url))
-      // @指向src
-      "@":resolve(__dirname,"./src")
-    }
-  },
-  plugins: [
-    vue(),
-    vueJsx(),
-    /** 将 SVG 静态图转化为 Vue 组件 */
-    svgLoader(),
-    /** SVG */
-    createSvgIconsPlugin({
-      iconDirs: [path.resolve(process.cwd(), 'src/icons/svg')],
-      symbolId: 'icon-[dir]-[name]'
-    })
-  ]
-})
+export default (configEnv: ConfigEnv): UserConfigExport => {
+  const viteENV = loadEnv(configEnv.mode, process.cwd())
+  const { VITE_PUBLIC_PATH } = viteENV
+  return {
+    base: VITE_PUBLIC_PATH,
+    resolve: {
+      alias: {
+        // @指向src
+        '@': resolve(__dirname, './src')
+      }
+    },
+    server: {
+      /** 是否开启 HTTPS */
+      https: false,
+      /** 设置 host: true 才可以使用 Network 的形式，以 IP 访问项目 */
+      host: true, // host: "0.0.0.0"
+      /** 端口号 */
+      port: 3333,
+      /** 是否自动打开浏览器 */
+      open: false,
+      /** 跨域设置允许 */
+      cors: true,
+      /** 端口被占用时，是否直接退出 */
+      strictPort: false,
+      /** 接口代理 */
+      proxy: {
+        '/': {
+          target: 'http://www.url.com',
+          ws: true,
+          /** 是否允许跨域 */
+          changeOrigin: true,
+          rewrite: (path) => path.replace('^/', '')
+        }
+      }
+    },
+    plugins: [
+      vue(),
+      vueJsx(),
+      /** 将 SVG 静态图转化为 Vue 组件 */
+      svgLoader(),
+      /** SVG */
+      createSvgIconsPlugin({
+        iconDirs: [path.resolve(process.cwd(), 'src/icons/svg')],
+        symbolId: 'icon-[dir]-[name]'
+      })
+    ]
+  }
+}
